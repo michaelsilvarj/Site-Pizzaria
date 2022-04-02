d618cfcfe5067c6d466985ce651933928fda3ae1
Michael Silva <michaelsilv@gmail.com>
2022-04-01 23:35:57 -0300

    Criação do encurtador de função, criado duas const

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+//Encurtador função document .querySelector
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+
+
 //Mapeia a lista
 pizzaJson.map((item,index)=>{
     //Clona a model
-    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+    // Adiciona elemento
+    c('.pizza-area').append(pizzaItem);
 
 
 });
