51efee6edc68c81c65a1f4abd034ac7ad8b03cdc
Michael Silva <michaelsilv@gmail.com>
2022-04-02 16:47:03 -0300

    Adição no modal de compra, imagem dos itens comprado, o nome e a descrição do item

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ pizzaJson.map((item,index)=>{
     //Clona a model
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+
+
+        pizzaItem.setAttribute('data-key',index);
         pizzaItem.querySelector('.pizza-item--img img').src= item.img;
         pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}` ;
         pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name ;
@@ -15,6 +18,15 @@ pizzaJson.map((item,index)=>{
         pizzaItem.querySelector('a').addEventListener('click',(e)=>{
             e.preventDefault();
 
+            //seta o item mais proximo de pizza-item
+            let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+            c('.pizzaBig img').src = pizzaJson[key].img;
+            c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+            c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
+/*
+*/
             c('.pizzaWindowArea').style.opacity=0;
             c('.pizzaWindowArea').style.display='flex';
             setTimeout(()=>{
